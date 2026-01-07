4e6bc8c437183f135c4a15f792936a828983b61c
reloaj <124435749+reloaj@users.noreply.github.com>
2026-01-07 22:23:09 +1100

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,12 @@
+const db = [
+  {
+    "id": 1,
+    "name": "Стартовый букет",
+    "price": 1500,
+    "desc": "Тестовый товар. Обновите базу через бота (/update_site)",
+    "images": [
+      "https://telegra.ph/file/8b3f27473852875f1b53e.jpg"
+    ],
+    "stock": 10
+  }
+];
